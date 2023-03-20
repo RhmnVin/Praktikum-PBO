d45b3ed6c5f151189ad7956c3615ee27a50842a9
RhmnVin <98610053+RhmnVin@users.noreply.github.com>
2023-03-20 22:24:48 +0800

    Add files via upload

---
+++ 2109106016_Muhammad rahman_POSTTEST3/src/pemasukan_bulanan.java
@@ -0,0 +1,10 @@
+public class pemasukan_bulanan extends pemasukan {
+
+    public pemasukan_bulanan(int uang_masuk, int uang_sekarang, int tanggal, int bulan, int tahun) {
+        super(uang_masuk, uang_sekarang, tanggal, bulan, tahun);
+    }
+
+    public pemasukan_bulanan() {
+    }
+    
+}
---
+++ 2109106016_Muhammad rahman_POSTTEST3/src/pengeluaran_bulanan.java
@@ -0,0 +1,10 @@
+public class pengeluaran_bulanan extends pengeluaran{
+
+    public pengeluaran_bulanan(int tgl, int bln, int thn, int pengeluaran) {
+        super(tgl, bln, thn, pengeluaran);    
+        System.out.println("berhasil menambahkan data");
+    }
+    public pengeluaran_bulanan(){}
+
+    
+}
